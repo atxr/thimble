33d8cdeb7d6c2ac3946fd38ba79b30c7cb2e7670
atxr <alexandre.tullot@gmail.com>
2022-08-05 14:09:20 +0200

    Implement toBytesVault
    Converts a FuzzyVaultBake into a byte representation

--- src/FuzzyVaultBake.cpp
+++ src/FuzzyVaultBake.cpp
@@ -1,4 +1,7 @@
+#include <iostream>
 #include <thimble/finger/FuzzyVaultBake.h>
+
+using namespace std;
 
 FuzzyVaultBake::FuzzyVaultBake(int width, int height, int dpi) : ProtectedMinutiaeTemplate(width, height, dpi) {}
 FuzzyVaultBake::FuzzyVaultBake(BytesVault bv)
@@ -6,3 +9,22 @@ FuzzyVaultBake::FuzzyVaultBake(BytesVault bv)
     fromBytes(bv.data, bv.size);
 }
 
+BytesVault FuzzyVaultBake::toBytesVault()
+{
+    uint8_t *data;
+    int size, wsize;
+
+    // Initialize byte array ...
+    size = getSizeInBytes();
+    data = (uint8_t *)malloc(size * sizeof(uint8_t));
+    if (data == NULL)
+    {
+        cerr << "byteFuzzyVault: "
+             << "out of memory." << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    // ... in which this protected minutiae template is packed.
+    toBytes(data);
+    return BytesVault(data, size);
+}
